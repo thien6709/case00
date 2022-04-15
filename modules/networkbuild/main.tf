c4fa17933cb4bf98740f350bff514408e5debff8
Thien NGUYEN <thi.nguyen@linkbynet.com>
2022-04-15 14:13:52 +0700

    tests

--- modules/networkbuild/main.tf
+++ modules/networkbuild/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["${var.address_space}"]
   resource_group_name = azurerm_resource_group.network.name
   dns_servers         = var.dns_servers
-  tags                = var.tags
+
 }
 
 resource "azurerm_subnet" "subnet" {
